75050f019fff14180afbb2f2043855a349fd23ae
Felice Zingaro <felice.zingaro@gmail.com>
2025-04-13 23:06:27 +0200

    merge

---
+++ ios/Pods/Target Support Files/flutter_sharing_intent/flutter_sharing_intent-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FlutterSharingIntentPlugin.h"
+
+FOUNDATION_EXPORT double flutter_sharing_intentVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_sharing_intentVersionString[];
+
